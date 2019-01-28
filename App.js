cbd090475291fde4c593c3c156e3f09fae7f21e4
emkristo <emkristovich@ucdavis.edu>
2019-01-28 08:26:44 -0800

    First commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Platform, StatusBar, StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
+import * as firebase from 'firebase';
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import Loading from './screens/Loading';
+import HomeScreen from './screens/HomeScreen';
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDv2e6dTo4qtQhJ4X5XgT3Wr6TzBbrXorg",
+    authDomain: "expo-insta-app.firebaseapp.com",
+    databaseURL: "https://expo-insta-app.firebaseio.com",
+    projectId: "expo-insta-app",
+    storageBucket: "expo-insta-app.appspot.com",
+    messagingSenderId: "713043675850"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+const MainStack = createStackNavigator(
+  {
+    HomeScreen: HomeScreen,
+    Loading: Loading,
+    Login: Login,
+    Signup: Signup,
+  },
+  {
+    initialRouteName: 'Loading',
+  }
+);
+
+const AppContainer = createAppContainer(MainStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
