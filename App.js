d4ba6c9ae7a7a9d76f0271be26626b4a1ef1d4cb
emkristo <emkristovich@ucdavis.edu>
2019-01-29 19:08:35 -0800

    added sources

--- App.js
+++ App.js
@@ -12,7 +12,10 @@ import HomeScreen from './screens/HomeScreen';
 //expo note: If LAN doesn't work, look at your Wifi IPv4 and set it using
 //set REACT_NATIVE_PACKAGER_HOSTNAME=<IPv4>
 //but for some reason this only seems to work at my house and on campus I have to use a tunnel
-//I also often connect my device physically 
+//I also often connect my device physically
+
+// Sources:
+// https://github.com/faahmad/react-native-firebase-auth
 
 // Initialize Firebase
   var config = {
